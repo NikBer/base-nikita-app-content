8292cee1f0530e06d54bc78b59619bb0c6bc67a8
Nikita <nikitaber@gmail.com>
2023-09-06 22:50:37 -0400

    change shop session changes

--- web/app/jobs/blog_id_fetcher_job.rb
+++ web/app/jobs/blog_id_fetcher_job.rb
@@ -1,22 +1,24 @@
 class BlogIdFetcherJob < ActiveJob::Base
   
-    def perform(store_id)
-      # Find the store
-      store = Store.find(store_id)
-  
-      # Return if the blog_id is already set
-      return if store.blog_id.present?
+  def perform(store_id)
+    # Find the store
+    store = Store.find(store_id)
 
-      # Create a new Shopify client using the shop's token
-      client = ShopifyAPI::Clients::Rest::Admin.new(session: shop.shopify_token)
+    # Return if the blog_id is already set
+    return if store.blog_id.present?
 
-      # Fetch the list of blogs using the new client
-      response = client.get(path: "blogs")
-      blogs = response.body
-      
-      # Set the store's blog_id to the ID of the last blog in the list
-      store.update!(blog_id: blogs.last["id"])
+# Find the corresponding shop for authentication
+shop = Shop.find_by(shopify_domain: store.shopify_domain)
 
-    end
+# Create a new Shopify client using the shop's token
+client = ShopifyAPI::Clients::Rest::Admin.new(session: shop.shopify_token)
+
+
+    # Fetch the list of blogs using the new client
+    response = client.get(path: "blogs")
+    blogs = response.body
+
+    # Set the store's blog_id to the ID of the last blog in the list
+    store.update!(blog_id: blogs.last["id"])
   end
-  
+end
--- web/app/services/blog_post_publisher_service.rb
+++ web/app/services/blog_post_publisher_service.rb
@@ -1,35 +1,33 @@
 class BlogPostPublisherService
-    def publish_blog_post(blog_post)
-      # Find the corresponding shop for authentication
-      shop = Shop.find_by(shopify_domain: blog_post.store.shopify_domain)
-      
-      # Create a new Shopify client using the shop's token
-      client = ShopifyAPI::Clients::Rest::Admin.new(session: shop.shopify_token)
+  def publish_blog_post(blog_post)
+    # Find the corresponding shop for authentication
+    shop = Shop.find_by(shopify_domain: blog_post.store.shopify_domain)
 
-      # Build your post request body for the article
-      body = {
-        article: {
-          title: blog_post.title,
-          body_html: blog_post.content,
-          published: true
-        }
+    # Create a new Shopify client using the shop's token
+    client = ShopifyAPI::Clients::Rest::Admin.new(session: shop.shopify_token)
+
+    # Build your post request body for the article
+    body = {
+      article: {
+        title: blog_post.title,
+        body_html: blog_post.content,
+        published: true
       }
+    }
 
-# Use `client.post` to send your request to the specified Shopify Admin REST API endpoint.
-response = client.post({
-  path: "blogs/#{blog_post.store.blog_id}/articles",
-  body: body
-})
+    # Use `client.post` to send your request to the specified Shopify Admin REST API endpoint.
+    response = client.post({
+      path: "blogs/#{blog_post.store.blog_id}/articles",
+      body: body
+    })
 
-# Check the response for success
-if response.code == 201
-  Rails.logger.info("Successfully published blog post ID #{blog_post.id} to Shopify store ID #{blog_post.store_id}")
-else
-  Rails.logger.error("Failed to publish blog post ID #{blog_post.id} to Shopify store ID #{blog_post.store_id}: #{response.body['errors']}")
-end
-
-    rescue => e
-      Rails.logger.error("Error publishing blog post ID #{blog_post.id} to Shopify store ID #{blog_post.store_id}: #{e.message}")
+    # Check the response for success
+    if response.code == 201
+      Rails.logger.info("Successfully published blog post ID #{blog_post.id} to Shopify store ID #{blog_post.store_id}")
+    else
+      Rails.logger.error("Failed to publish blog post ID #{blog_post.id} to Shopify store ID #{blog_post.store_id}: #{response.body['errors']}")
     end
+  rescue => e
+    Rails.logger.error("Error publishing blog post ID #{blog_post.id} to Shopify store ID #{blog_post.store_id}: #{e.message}")
   end
-  
+end
